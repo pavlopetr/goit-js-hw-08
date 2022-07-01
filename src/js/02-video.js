fb5983c2013fb562c8e9432326a03b124dd67d75
pavlopetr <rezar66612@gmail.com>
2022-07-01 14:24:09 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -27,30 +27,3 @@ savedTimeVideo();
 player.on('timeupdate', throttle(setCurrentTimeVideo, 1000));
 
 
-// import throttle from 'lodash.throttle';
-// import Player from '@vimeo/player';
-
-// const iframe = document.querySelector('iframe');
-// const player = new Player(iframe);
-// const TIME_PROGRESS_KEY = 'videoplayer-current-time';
-
-// const setCurrentTimeVideo = evt => {
-//   const stringifyData = JSON.stringify(evt);
-
-//   localStorage.setItem(TIME_PROGRESS_KEY, stringifyData);
-// };
-
-// const setSavedTimeVideo = () => {
-//   const savedDataTime = localStorage.getItem(TIME_PROGRESS_KEY);
-//   const parsedDataTime = JSON.parse(savedDataTime) ?? {};
-
-//   if (savedDataTime) {
-//     player.setCurrentTime(parsedDataTime.seconds);
-//   }
-// };
-
-// setSavedTimeVideo();
-// player.on('timeupdate', throttle(setCurrentTimeVideo, 1000));
-
-
-
