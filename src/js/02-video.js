4fc687314ace0e518243628c085dc94c1feac262
pavlopetr <rezar66612@gmail.com>
2022-07-01 14:20:29 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -8,7 +8,7 @@ const player = new Player(iframe);
 
 const TIME_POINT_KEY = 'videoplayer-current-time';
 
-const currentTimeVideo = e => {
+const setCurrentTimeVideo = e => {
   const stringifyData = JSON.stringify(e);
 
   localStorage.setItem(TIME_POINT_KEY, stringifyData);
@@ -16,12 +16,41 @@ const currentTimeVideo = e => {
 
 const savedTimeVideo = () => {
   const savedDataTime = localStorage.getItem(TIME_POINT_KEY);
-  const parsDataTime = JSON.parse(savedDataTime) ?? {};
+  const parsedDataTime = JSON.parse(savedDataTime) ?? {};
 
   if (savedDataTime) {
-    player.currentTime(parsDataTime.seconds);
+    player.setCurrentTime(parsedDataTime.seconds);
   }
 };
 
 savedTimeVideo();
-player.on('timeupdate', throttle(currentTimeVideo, 1000));
+player.on('timeupdate', throttle(setCurrentTimeVideo, 1000));
+
+
+// import throttle from 'lodash.throttle';
+// import Player from '@vimeo/player';
+
+// const iframe = document.querySelector('iframe');
+// const player = new Player(iframe);
+// const TIME_PROGRESS_KEY = 'videoplayer-current-time';
+
+// const setCurrentTimeVideo = evt => {
+//   const stringifyData = JSON.stringify(evt);
+
+//   localStorage.setItem(TIME_PROGRESS_KEY, stringifyData);
+// };
+
+// const setSavedTimeVideo = () => {
+//   const savedDataTime = localStorage.getItem(TIME_PROGRESS_KEY);
+//   const parsedDataTime = JSON.parse(savedDataTime) ?? {};
+
+//   if (savedDataTime) {
+//     player.setCurrentTime(parsedDataTime.seconds);
+//   }
+// };
+
+// setSavedTimeVideo();
+// player.on('timeupdate', throttle(setCurrentTimeVideo, 1000));
+
+
+
